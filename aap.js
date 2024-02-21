cdbb11627a5cc5ee4249976b1ce840f9dc6b4587
Aqil Fasih <aqilfasihsb11@gmail.com>
2024-02-22 04:11:22 +0500

    Update

---
+++ aap.js
@@ -0,0 +1,43 @@
+// var greetings = "Hello World"  //string
+// var noOfStudents = 50 // number 
+// var isClassGoingOn = true // boolean
+// var noOfStudentsPassed //value will be undefined
+
+// alert(greetings)
+// alert(noOfStudents)
+// alert(isClassGoingOn)
+// alert(noOfStudentsPassed)
+
+
+// Q-1
+
+   var invalid = 'Error please enter a valid password'  
+
+alert(invalid)
+
+// Q-2
+
+   var coding = 'welcome to JS..Happy Coding'  
+
+alert(coding)
+
+
+// Q-3
+
+   var land = 'Welcome to LandJS'
+
+  alert(land)
+
+// Q-4
+
+     var HappyCoding = 'Happy Coding  prevent this page from creating additional dialog'
+        
+   alert(HappyCoding)
+
+
+// Q-5
+
+
+    var run = 'Hello... I can JS through my web browers'  
+
+    alert(run)
