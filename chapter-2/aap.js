192c16dd666d35839adfcd9d8691717127975037
Aqil Fasih <aqilfasihsb11@gmail.com>
2024-02-24 09:43:36 +0500

    done

---
+++ chapter-2/aap.js
@@ -0,0 +1,32 @@
+// Q-1
+
+var UserName = 'Aqil Fasih'  
+
+alert(UserName)
+
+// Q-2
+
+   var age = '26 years'   
+
+alert(age)
+
+
+// Q-3
+
+   var address = 'Noorani Basti Korangi 1-1/2 karachi'
+
+  alert(address)
+
+// Q-4
+
+     var Course = 'Web Mobile Application'
+        
+   alert(Course)
+
+
+// Q-5
+
+
+    var Techer = 'Sir Bilal Raza Attari'  
+
+    alert(Techer)
